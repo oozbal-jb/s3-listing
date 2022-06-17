ab839251a4b977fc14b5582ed4e64adc90d9b6d5
oozbal-jb <orhun.ozbal@jitterbit.com>
2022-06-18 02:47:38 +0300

    first working prototype

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 import boto3
 import os
 import urllib.parse
-from dotenv import load_dotenv
 
 def createHrefList(files,folders):
     hrefList=[]
@@ -15,109 +14,76 @@ def createHrefList(files,folders):
         hrefList.append(href)
         hrefStr=hrefStr+href
     return hrefStr
-
-load_dotenv()
-
-ACCESS_KEY=os.getenv('ACCESS_KEY_ID')
-SECRET_KEY=os.getenv('ACCESS_SECRET')
-REGION=os.getenv('REGION')
-client = boto3.client ( 's3',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY
-)
-#get value of prefix query param.
-safePrefix='appBuilder.jmx%2F'
-prefix=urllib.parse.unquote(safePrefix)
-print(prefix)
-
-bucket='lcap-jmeter-results'
-apiId='test-api'
+    
 
 
+def lambda_handler(event, context):
+    # TODO implement
+    ACCESS_KEY=os.getenv('ACCESS_KEY_ID')
+    SECRET_KEY=os.getenv('ACCESS_SECRET')
+    client = boto3.client ( 's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY
+    )
+    #get value of prefix query param.
+    safePrefix=event['query']['prefix']
 
-# Print out bucket names
-# for bucket in client.list_buckets()["Buckets"]:
-#     print(bucket["Name"])
-#     print("http://localhost:5000/search?bucket="+bucket["Name"])
+    prefix=urllib.parse.unquote(safePrefix)
+    host=event['headers']['Host']
+    REGION=event['query']['region']
 
+    bucket=event['query']['bucket']
 
-response=client.list_objects_v2(
+    path=event['path']
+    print(prefix)
+    print(safePrefix)
+    print(REGION)
+    print(host)
+    
+    print(event)
+    
+    
+#    REGION='us-east-2'
+#    bucket='lcap-jmeter-results'
+#    apiId='test-api'
+    response=client.list_objects_v2(
     Bucket=bucket,
     Delimiter='/',
     Prefix=prefix
-)
-
-# print(response)
-
-#get folders under prefix
-folders=[]
-try:
-    commonPrefixes=response['CommonPrefixes']
-    for i in commonPrefixes:
-        folderName=i['Prefix'].split('/')[-2]+"/"
-        urlPrefix=urllib.parse.quote(i['Prefix'], safe='')
-        link="https://"+apiId+".execute-api."+REGION+".amazonaws.com/search?prefix="+urlPrefix+"&"+bucket
-        folders.append( (folderName,link) )
-    print("folders    ",folders)
-except:
-    print("There is a problem in response CommonPrefixes.")
-
-
-#get files under prefix
-files=[]
-try:
-    contents=response['Contents']
-    for e in contents:
-        key=e['Key'].split('/')[-1]
-        link="https://"+bucket+".s3."+REGION+".amazonaws.com/"+key
-        files.append( ( key , link ) )
-    print("files   ",files)
-except:
-    print("There s a problem in response Contents.")
-
-hrefList=createHrefList(files,folders)
-
-htmlResponse="""<html><body>
-<h2>"""+bucket+"""</h2>
-<ul>
-"""+hrefList+"""
-</ul>
-</body>
-</html>
-"""
-
-print(htmlResponse)
-
-
-
-
-
-
-
-
-# @app.route('/', methods=['GET'])
-# def home():
-#     return '''<h1>Distant Reading Archive</h1>
-# <p>A prototype API for distant reading of science fiction novels.</p>'''
-
-
-
-# @app.route('/search', methods=['GET'])
-# def bucket_root():
-#     if 'bucket' in request.args:
-#         bucketName = request.args['bucket']
-#         session = boto3.Session (
-#             aws_access_key_id=ACCESS_KEY,
-#             aws_secret_access_key=SECRET_KEY
-#         )
-#         s3 = session.resource('s3')
-#         bucket = s3.Bucket(bucketName)
-#         objs = bucket.objects.all()
-#         for obj in objs:
-#             print(obj)
-#         return "success"
-#     else:
-#         return "Error: No bucket field provided. Please provide bucket name."
-
-
-# app.run()
+    )
+    #get folders under prefix
+    folders=[]
+    try:
+        commonPrefixes=response['CommonPrefixes']
+        for i in commonPrefixes:
+            folderName=i['Prefix'].split('/')[-2]+"/"
+            urlPrefix=urllib.parse.quote(i['Prefix'], safe='')
+            link="https://"+host+path+"?prefix="+urlPrefix+"&bucket="+bucket+"&region="+REGION+""
+            folders.append( (folderName,link) )
+        print("folders    ",folders)
+    except:
+        print("There is a problem in response CommonPrefixes.")
+    
+    
+    #get files under prefix
+    files=[]
+    try:
+        contents=response['Contents']
+        for e in contents:
+            key=e['Key'].split('/')[-1]
+            link="https://"+bucket+".s3."+REGION+".amazonaws.com/"+e['Key']
+            files.append( ( key , link ) )
+        print("files   ",files)
+    except:
+        print("There s a problem in response Contents.")
+    hrefList=createHrefList(files,folders)
+    
+    htmlResponse="""<html><body>
+    <h2>"""+bucket+"""</h2>
+    <ul>
+    """+hrefList+"""
+    </ul>
+    </body>
+    </html>
+    """
+    return htmlResponse
